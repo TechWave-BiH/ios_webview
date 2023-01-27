d50e67ec01a7affb12697cc4b3137f30289502b2
tijl verbert <tijl.verbert@gmail.com>
2023-01-27 13:10:18 +0100

    remove tag

--- testWebView/ViewController.swift
+++ testWebView/ViewController.swift
@@ -28,11 +28,6 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate {
         """
         <html><head><meta name=\"viewport\" content=\"width=device-width, maximum-scale=1, user-scalable=yes, initial-scale=1.0\"><title>Video Test / EMP-SAM / 15/01/23</title></head><body style=\"margin: auto;\"><center><ins data-do=\"app.legaseriea.it\" data-domain=\"techwave.io\" data-id=\"6d3674c49af4071e414d4adbab038085\" data-type=\"video-template-single\" data-video=\"lazio-milan-21-01-2023\"></ins><script async src=\"https://bb1.broadbandy.net/display/app.js\"></script></center></body></html>
         """
-        /*
-        if let url = Bundle.main.url(forResource: "tag", withExtension: "html") {
-            webView.load(URLRequest(url: url))
-        }
-         */
         guard let baseURL = URL(string: "https://app.legaseriea.it") else { return }
         webView.loadHTMLString(embeddedCode, baseURL: baseURL)
         
@@ -43,19 +38,6 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate {
         webView.frame = view.bounds;
     }
     
-    /*func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-        if navigationAction.navigationType == .linkActivated  {
-            if let url = navigationAction.request.url {
-                UIApplication.shared.open(navigationAction.request.url!, options: [:], completionHandler: nil) //UIApplication.shared.open(url)
-                print(url)
-                decisionHandler(.cancel)
-            } else {
-                decisionHandler(.allow)
-            }
-        } else {
-            decisionHandler(.allow)
-        }
-    }*/
     func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
         UIApplication.shared.open(navigationAction.request.url!, options: [:], completionHandler: nil)
         return nil
